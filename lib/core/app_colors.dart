1ba528759a4c3e492f9587b7e25896e56634ee05
AQ <cshazemelwazery@gmail.com>
2025-07-23 18:21:53 +0300

    finished app colors

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static Color darkBlue = const Color(0xFF242C3B);
-  static Color lightBlue = const Color(0xFF3883D1);
+  static Color lightBlue = const Color(0xFF3F8EEB);
+  static Color lightBlue2 = const Color(0xFF4661ED);
   static Color white = const Color(0xFFFFFFFF);
   static Color black = const Color(0xFF000000);
 }
