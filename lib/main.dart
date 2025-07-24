4ebbdeae5f0bd05ae53258671249758c8b182e05
AQ <cshazemelwazery@gmail.com>
2025-07-24 05:42:42 +0300

    readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'home/screens/home_screen.dart';
 
-void main() {
-  runApp(const OnlineBikeShoppingApp());
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  runApp(DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => OnlineBikeShoppingApp(), // Wrap your app
+  ), );
 }
 
 class OnlineBikeShoppingApp extends StatelessWidget {
